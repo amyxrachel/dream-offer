2fd6b889e3d7e6ed778c6eb432470d5a768d5105
amyxrachel <amyxrach@gmail.com>
2026-09-18 13:50:35 +0530

    Time: 438 ms (11.29%) | Memory: 44 MB (10.46%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,10 @@
+import java.util.regex.Pattern;
+class Solution {
+    public boolean isMatch(String s, String p) 
+    {
+        if(s.matches(p)){
+            return true;
+        }
+        return false;
+    }
+}
